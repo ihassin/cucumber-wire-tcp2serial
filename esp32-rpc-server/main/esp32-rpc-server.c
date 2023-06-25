527a99284ec8fe22c406942ae1f12920609b4e31
Adrian McEwen <adrianm@mcqn.com>
2023-06-25 11:23:36 +0100

    Updates so esp32-rpc-server builds with the latest esp-idf.

--- esp32-rpc-server/main/esp32-rpc-server.c
+++ esp32-rpc-server/main/esp32-rpc-server.c
@@ -49,7 +49,7 @@ static void cucumber_task(void *arg)
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
+        .source_clk = UART_SCLK_APB,
     };
     int intr_alloc_flags = 0;
 
